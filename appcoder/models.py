c35b70e62bfc482ff31095959af2fe92fd596cc4
unknown <jmrivera82@gmail.com>
2023-09-06 08:16:07 -0300

    clase python hasta la 22 - avance proyecto

--- appcoder/models.py
+++ appcoder/models.py
@@ -28,3 +28,40 @@ class Entregable(models.Model):
     nombre=models.CharField(max_length=50)
     fecha_entrega=models.DateField()
     entregado=models.BooleanField()
+    
+    
+
+#clases para proyecto final
+#usuarios
+
+#Cursos 
+class Oficinas(models.Model): 
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=30)
+    telefono=models.IntegerField()
+    
+#estudiantes
+class Usuarios(models.Model):
+    nombre=models.CharField(max_length=50)
+    oficina=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=50)
+    email=models.EmailField()
+    
+#entregables
+class Trabajos(models.Model):
+    solicitud=models.CharField(max_length=50)
+    lugar=models.CharField(max_length=50)
+    fecha_reporte=models.DateTimeField()
+    fecha_realiza=models.DateTimeField()
+    entregado=models.BooleanField()
+
+#profesores
+class Equipos(models.Model):
+    tipoequipo=models.CharField(max_length=50)
+    marca=models.CharField(max_length=50)
+    modelo=models.CharField(max_length=50)
+    email=models.EmailField()
+
+    def __str__ (self):
+        return f"{self.nombre} - {self.apellido}"
+    
